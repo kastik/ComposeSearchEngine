e69fec2d754f3ef546032905f14386783ba15aba
kastik <kostas21422112@gmail.com>
2025-01-10 19:06:41 +0200

    Create a greek report

--- searchenginedocs/build.gradle.kts
+++ searchenginedocs/build.gradle.kts
@@ -19,4 +19,18 @@ latex {
          * Can be passed files, strings, or any Object compatible with Gradle's project.files
          */
     }
+    terminalEmulator.set("zsh") // Your terminal
+    waitTime.set(1) // How long before considering a process stalled
+    waitUnit.set(TimeUnit.MINUTES) // Time unit for the number above
+    pdfLatexCommand.set("lulatex")
+    bibTexCommand.set("bibtex")
+    "src/report_greek" {
+        // Options for pdflatex
+        extraArguments = listOf("-shell-escape", "-synctex=1", "-interaction=nonstopmode") //"-halt-on-error")
+
+        /*
+         * Additional files and directories whose change should trigger a build in case gradle is used with -t flag.
+         * Can be passed files, strings, or any Object compatible with Gradle's project.files
+         */
+    }
 }
